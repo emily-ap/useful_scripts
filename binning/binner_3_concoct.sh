abcf89dfdd94656dcdcafc33cb2047dae5d7f4d8
Emily Aguilar-Pine <102376315+emily-ap@users.noreply.github.com>
2025-02-13 15:19:37 -0600

    Update binner_3_concoct.sh

--- binning/binner_3_concoct.sh
+++ binning/binner_3_concoct.sh
@@ -11,38 +11,36 @@
 
 #3 arg is your path to the specified mapping files - these need to be all in their own directory  - DO NOT END this path in a slash, these should be sorted bams and their respecitve indexes, sorted bam endsing should be *-sorted.bam
 
-#4 arg is your output directory for the binning of that sample, do not put a slash at the end of this
+#4 arg is your max threadcount 
 
-#5 arg is your max threadcount 
-
-#$6 arg is your min contig length to be considered for binning
+#5 arg is your min contig length to be considered for binning
 
 ## first test if the output directory already exists and if not --> make it 
 
-test -d $4 || mkdir -p $4
-test -d $4/binners || mkdir -p $4/binners
-mkdir $4/binners/concoct
+test -d binning_out || mkdir -p binning_out
+test -d binning_out/binners || mkdir -p binning_out/binners
+mkdir binning_out/binners/concoct
 
-echo "Beginning to run concoct on $1 with a min. contig size of $6 and using the assembly file: $2. Coverage files will be taken from the $3 directory and all results will be put in the generated $4 directory." 
+echo "Beginning to run concoct on $1 with a min. contig size of $6 and using the assembly file: $2. Coverage files will be taken from the $3 directory and all results will be put in the generated binning_out directory." 
 
 ## CONCOCT SECTION
 
-mkdir $4/binners/concoct/tmp_concoct_files
-mkdir $4/binners/concoct/bin_tmp
-mkdir $4/binners/concoct/BINS
+mkdir binning_out/binners/concoct/tmp_concoct_files
+mkdir binning_out/binners/concoct/bin_tmp
+mkdir binning_out/binners/concoct/BINS
 
-cut_up_fasta.py $2 -c 10000 -o 0 --merge_last -b $4/binners/concoct/tmp_concoct_files/$1-10K.bed > $4/binners/concoct/tmp_concoct_files/$1-10K.fna &&
+cut_up_fasta.py $2 -c 10000 -o 0 --merge_last -b binning_out/binners/concoct/tmp_concoct_files/$1-10K.bed > binning_out/binners/concoct/tmp_concoct_files/$1-10K.fna &&
 
 
-concoct_coverage_table.py $4/binners/concoct/tmp_concoct_files/$1-10K.bed $3/*.bam >> $4/binners/concoct/tmp_concoct_files/coverage_table.tsv &&
+concoct_coverage_table.py binning_out/binners/concoct/tmp_concoct_files/$1-10K.bed $3/*.bam >> binning_out/binners/concoct/tmp_concoct_files/coverage_table.tsv &&
   
 
-concoct --threads $5 --length_threshold $6 --composition_file $4/binners/concoct/tmp_concoct_files/$1-10K.fna --coverage_file $4/binners/concoct/tmp_concoct_files/coverage_table.tsv -b $4/binners/concoct/bin_tmp/ &&
+concoct --threads $5 --length_threshold $6 --composition_file binning_out/binners/concoct/tmp_concoct_files/$1-10K.fna --coverage_file binning_out/binners/concoct/tmp_concoct_files/coverage_table.tsv -b binning_out/binners/concoct/bin_tmp/ &&
 
 
-merge_cutup_clustering.py $4/binners/concoct/bin_tmp/clustering_gt$6.csv >> $4/binners/concoct/bin_tmp/clustering_merged.csv &&
+merge_cutup_clustering.py binning_out/binners/concoct/bin_tmp/clustering_gt$6.csv >> binning_out/binners/concoct/bin_tmp/clustering_merged.csv &&
 
 
-extract_fasta_bins.py $2 $4/binners/concoct/bin_tmp/clustering_merged.csv --output_path $4/binners/concoct/BINS &&
+extract_fasta_bins.py $2 binning_out/binners/concoct/bin_tmp/clustering_merged.csv --output_path binning_out/binners/concoct/BINS &&
 
 echo "concoct section has finished!"
