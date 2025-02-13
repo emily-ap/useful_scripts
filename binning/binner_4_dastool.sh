13ae329db56819947f9e496194d7cd2cd81a60b9
Emily Aguilar-Pine <102376315+emily-ap@users.noreply.github.com>
2025-02-13 16:31:14 -0600

    Update binner_4_dastool.sh

--- binning/binner_4_dastool.sh
+++ binning/binner_4_dastool.sh
@@ -9,27 +9,41 @@
 
 #2 arg is your assembly 
 
-#3 arg is path to your miniconda directory (i.e., something like /home/eaguilarpine/miniconda3) - do not end this in a slash!
+#3 arg is path to your Fasta_to_Contig2Bin.sh script, if you're using miniconda it's probably something like this "/home/user/miniconda3/envs/dastool/bin/Fasta_to_Contig2Bin.sh" 
 
 #4 arg is your max threadcount 
+
+#5 your chosen search engine (usearch, diamond, blastp)
 
 ## first test if the output directory already exists and if not --> make it 
 
 test -d dastool_${1}_out || mkdir -p dastool_${1}_out
 test -d dastool_${1}_out/prepfiles || mkdir -p dastool_${1}_out/prepfiles
 
-echo "Beginning to run DASTool on $1." 
-
 ## establish where the binners output bins are 
 
-mkdir binning_out/binners/concoct/tmp_concoct_files
-mkdir binning_out/binners/concoct/bin_tmp
-mkdir binning_out/binners/concoct/BINS
+concoctBins=binning_out/binners/concoct/BINS
+maxbin2Bins=binning_out/binners/maxbin2/bins/maxbin2_markerset40_bin
+metabat2Bins=binning_out/binners/metabat2
 
-# maxbin2 step 0 --> prepare the directories 
+echo "Beginning to run DASTool on $1. Pulling bins from \n${concoctBins} , \n${maxbin2Bins} , \n${metabat2Bins}." 
 
-mkdir binning_out/binners/maxbin2/abundance
-mkdir binning_out/binners/maxbin2/bins
-mkdir binning_out/binners/maxbin2/bins/maxbin2_markerset40_bin
+## Let's start prepping the tsv files required for Dastool
+
+maxbintsv=dastool_${1}_out/prepfiles/${1}-maxbin2_scaffold2bin.tsv
+concocttsv=dastool_${1}_out/prepfiles/${1}-concoct_scaffold2bin.tsv
+metabattsv=dastool_${1}_out/prepfiles/${1}-metabat2_scaffold2bin.tsv
+
+$3 -e fna -i $maxbin2Bins > $maxbintsv &&
+$3 -e fna -i $concoctBins > $concocttsv &&
+$3 -e fna -i $metabat2Bins > $metabattsv &&
+echo "Fasta_to_Scaffolds2Bin.sh is finished for sample ${1}!"
+
+## run dastool 
+
+DAS_Tool --score_threshold=0.5 -i $concocttsv,$maxbintsv,$metabattsv -l concoct,maxbin2,metabat2 -c $2 -o dastool/${1}-dastool_${5} --threads $4 --write_bins --search_engine $5
+
+
+
 
 
