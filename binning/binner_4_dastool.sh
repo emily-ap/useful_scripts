b4eb61ad93b6ef567ba68b16cd2de99f7599ad73
Emily Aguilar-Pine <102376315+emily-ap@users.noreply.github.com>
2025-02-13 23:06:42 -0600

    Update binner_4_dastool.sh

--- binning/binner_4_dastool.sh
+++ binning/binner_4_dastool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### this is to be run once other binning scripts (binner_1-3) have been run. 
-## requirements, dastool version, R (>v4, =v3 is good), pullseq, diamond or blastp or usearch (for search engine)
+## requirements, dastool, rename command from linux util, R (>v4, =v3 is good), pullseq, diamond or blastp or usearch (for search engine)
 # This script will run DasTool using all the output bins from binners 1-3 (metabat2, maxbin2, concoct) 
 # Run this in the directory where you're "binning_out" directory is!!!
 
@@ -19,14 +19,19 @@
 
 test -d dastool_${1}_out || mkdir -p dastool_${1}_out
 test -d dastool_${1}_out/prepfiles || mkdir -p dastool_${1}_out/prepfiles
+test -d dastool_${1}_out/final_out_${1} || mkdir -p dastool_${1}_out/final_out_${1}
 
-## establish where the binners output bins are 
+## establish where the binners output bins are and make sure all bins end in ".fna"
 
 concoctBins=binning_out/binners/concoct/BINS
-maxbin2Bins=binning_out/binners/maxbin2/bins/maxbin2_markerset40_bin
+maxbin2Bins=binning_out/binners/maxbin2/bins
 metabat2Bins=binning_out/binners/metabat2
 
-echo "Beginning to run DASTool on $1. Pulling bins from \n${concoctBins} , \n${maxbin2Bins} , \n${metabat2Bins}." 
+rename "fa" "fna" $metabat2Bins/*.fa && 
+rename "fasta" "fna" $maxbin2Bins/*.fasta && 
+rename "fa" "fna" $concoctBins/*.fa &&
+
+echo "Beginning to run DASTool on $1. Pulling bins from ${concoctBins} , ${maxbin2Bins} , ${metabat2Bins}." &&
 
 ## Let's start prepping the tsv files required for Dastool
 
@@ -41,11 +46,6 @@ echo "Fasta_to_Scaffolds2Bin.sh is finished for sample ${1}!" &&
 
 ## run dastool 
 
-DAS_Tool --score_threshold=0.5 -i $concocttsv,$maxbintsv,$metabattsv -l concoct,maxbin2,metabat2 -c $2 -o dastool/${1}-dastool_${5} --threads $4 --write_bins --search_engine $5 && 
+DAS_Tool --score_threshold=0.5 -i $concocttsv,$maxbintsv,$metabattsv -l concoct,maxbin2,metabat2 -c $2 -o dastool_${1}_out/final_out_${1} --threads $4 --write_bins --search_engine $5 && 
 
 echo "DASTool has finished for sample ${1}!"
-
-
-
-
-
