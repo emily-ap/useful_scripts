4936d360970ff0da10bdc34306b2ab849fa1050a
Emily Aguilar-Pine <102376315+emily-ap@users.noreply.github.com>
2025-02-14 15:02:52 -0600

    Update binning_5_rename_to_final_bins.sh

--- binning/binning_5_rename_to_final_bins.sh
+++ binning/binning_5_rename_to_final_bins.sh
@@ -35,7 +35,7 @@ sed -i 's/\s//g' ${1}_4.txt
 paste -d" " ${1}_1.txt ${1}_4.txt >> ${1}_rename.csv
 sed -i 's/\s//g' ${1}_rename.csv
 sed -i "s|^|${3}/|g" ${1}_rename.csv #use double quotes to make the shell expand variables 
-sed -i "s|,|, ${3}/|g" ${1}_rename.csv
+sed -i "s|,|,\s${3}/|g" ${1}_rename.csv
 
 ## remove reference files to get renaming file 
 rm ${1}_1.txt 
